0361d116f342bc2385d6284cdc859fb279d20c88
sohumdalal <dsohum@gmail.com>
2024-10-14 16:35:27 -0400

    added CSV

--- Standard/program.java
+++ Standard/program.java
@@ -3,6 +3,11 @@ public class program
 
 {
     public static void main(String[] args) {
-        System.out.println(Standard.ReadCSVText("Stocks.csv"));
+       String [][] myData = Standard.ReadCSVText("stock_data.csv");
+       System.out.println(myData[0]);
+       System.out.println(myData.length);
+       System.out.println(myData[2][2]);
+
+
     }
 }
