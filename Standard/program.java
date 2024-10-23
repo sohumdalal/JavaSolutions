97aa18e725a074709c4d2cda1290a85b7e4ddaa1
sohumdalal <dsohum@gmail.com>
2024-10-23 18:08:46 -0400

    running

--- Standard/program.java
+++ Standard/program.java
@@ -3,9 +3,6 @@ public class program
 
 {
     public static void main(String[] args) {
-       String [][] myData = Standard.ReadCSVText("stock_data.csv");
-       System.out.println(myData[0]);
-       System.out.println(myData.length);
-       System.out.println(myData[2][2]);
+       System.out.println(Standard.isPrime(20));
     }
 }
