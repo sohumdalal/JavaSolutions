6c07c71d421744aab361298a911ab76991dbb998
sohumdalal <dsohum@gmail.com>
2024-11-03 15:17:14 -0500

    prac problems solutions

--- Standard/program.java
+++ Standard/program.java
@@ -5,4 +5,11 @@ public class program
     public static void main(String[] args) {
        System.out.println(Standard.isPrime(20));
     }
+
+    // public static void main(String[] args) {
+    //     int[] nums = {2, 7, 11, 15};
+    //     int target = 9;
+    //     int[] result = twoSum(nums, target);
+    //     System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    // }
 }
